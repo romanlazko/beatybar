2b00a6d1e611c8d6dbf0bd7be020537a06ef051b
romanlazko <romanlazko@gmail.com>
2023-08-04 13:26:43 +0200

    Исправлен баг в элементе меню

--- app/Bots/brno_beauty_bar_bot/resources/views/components/responsive-nav-links.blade.php
+++ app/Bots/brno_beauty_bar_bot/resources/views/components/responsive-nav-links.blade.php
@@ -1,10 +1,10 @@
 
-<x-responsivenav-link :href="route('brno_beauty_bar_bot.client.index')" :active="request()->routeIs('brno_beauty_bar_bot.client.*')">
+<x-responsive-nav-link :href="route('brno_beauty_bar_bot.client.index')" :active="request()->routeIs('brno_beauty_bar_bot.client.*')">
     {{ __('Clients') }}
-</x-responsivenav-link>
-<x-responsivenav-link :href="route('brno_beauty_bar_bot.schedule.index')" :active="request()->routeIs('brno_beauty_bar_bot.schedule.*')">
+</x-responsive-nav-link>
+<x-responsive-nav-link :href="route('brno_beauty_bar_bot.schedule.index')" :active="request()->routeIs('brno_beauty_bar_bot.schedule.*')">
     {{ __('Schedule') }}
-</x-responsivenav-link>
-<x-responsivenav-link :href="route('brno_beauty_bar_bot.appointment.index')" :active="request()->routeIs('brno_beauty_bar_bot.appointment.*')">
+</x-responsive-nav-link>
+<x-responsive-nav-link :href="route('brno_beauty_bar_bot.appointment.index')" :active="request()->routeIs('brno_beauty_bar_bot.appointment.*')">
     {{ __('Appointments') }}
-</x-responsivenav-link>
+</x-responsive-nav-link>
