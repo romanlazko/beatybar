b2281d153e8c9e8ebcc802433e3969fb76344355
romanlazko <romanlazko@gmail.com>
2023-12-18 15:34:14 +0100

    Создание логики для экспорта appointments

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Romanlazko\Telegram\Models\Bot;
 use Romanlazko\Telegram\Providers\TelegramServiceProvider;
+use App\Bots\valeri_beautybar_bot\Models\Appointment;
+use App\Bots\valeri_beautybar_bot\Models\AppointmentExport;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +20,22 @@ use Romanlazko\Telegram\Providers\TelegramServiceProvider;
 
 Route::get('/', function () {
     return redirect()->route('dashboard');
+});
+
+Route::get('/export_appointment', function () {
+    Appointment::all()->each(function ($appointment) {
+        AppointmentExport::create([
+            'client_id' => $appointment->client_id,
+            'employee_id' => 1,
+            'service_id' => 1,
+            'date' => $appointment->schedule->date,
+            'term' => $appointment->schedule->term,
+            'price' => $appointment->price,
+            'status' => $appointment->status,
+            'created_at' => $appointment->created_at,
+            'updated_at' => $appointment->updated_at,
+        ]);
+    });
 });
 
 Route::get('/dashboard', function () {
