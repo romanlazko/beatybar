54eb0710e9085ce72cd1e4b6ce38de5739b2a498
romanlazko <romanlazko@gmail.com>
2023-12-18 15:37:14 +0100

    Исправление ошибки имени модели

--- app/Bots/valeri_beautybar_bot/Models/AppointmentExport.php
+++ app/Bots/valeri_beautybar_bot/Models/AppointmentExport.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Appointment extends Model
+class AppointmentExport extends Model
 {
     use HasFactory, SoftDeletes;
 
